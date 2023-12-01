43822b5b591402060a0d37d48460865a60c1ecec
Pitsco <derrickhuang2@gmail.com>
2023-11-30 18:03:17 -0800

    commenting out API for deployment for now

--- src/main/java/com/nighthawk/spring_portfolio/mvc/sortingalgs/SortingAlgs.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/sortingalgs/SortingAlgs.java
@@ -5,24 +5,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class DogBreedsApiClient {
+// public class DogBreedsApiClient {
 
-    public static void main(String[] args) {
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://dog-breeds2.p.rapidapi.com/dog_breeds"))
-                    .header("X-RapidAPI-Key", "YOUR-RAPIDAPI-KEY")
-                    .header("X-RapidAPI-Host", "dog-breeds2.p.rapidapi.com")
-                    .method("GET", HttpRequest.BodyPublishers.noBody())
-                    .build();
+//     public static void main(String[] args) {
+//         try {
+//             HttpRequest request = HttpRequest.newBuilder()
+//                     .uri(URI.create("https://dog-breeds2.p.rapidapi.com/dog_breeds"))
+//                     .header("X-RapidAPI-Key", "YOUR-RAPIDAPI-KEY")
+//                     .header("X-RapidAPI-Host", "dog-breeds2.p.rapidapi.com")
+//                     .method("GET", HttpRequest.BodyPublishers.noBody())
+//                     .build();
 
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             
-            // Print the response body
-            System.out.println("Response Code: " + response.statusCode());
-            System.out.println("Response Body: " + response.body());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+//             // Print the response body
+//             System.out.println("Response Code: " + response.statusCode());
+//             System.out.println("Response Body: " + response.body());
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
