40b4b946de1a95abff4cb6112d5e11d1a5efbdeb
JishnuS420 <broburn123@gmail.com>
2023-12-11 03:40:27 -0800

    adding an origin

--- src/main/java/com/nighthawk/spring_portfolio/mvc/fibonacci/FibonacciController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/fibonacci/FibonacciController.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api/fibonacci")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://127.0.0.1:4200"})
 public class FibonacciController {
 
     @GetMapping("/for/{num}")
